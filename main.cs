f1c141806f7ef5014f381212b2849015ee7394bf
Selisine <Aidenpike113@gmail.com>
2023-03-24 14:01:55 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,36 @@
+using System;
+
+class Program {
+    static int cpuChoice(){
+        Random rand = new Random(); 
+        
+        return rand.Next(3);
+    }
+
+    string cpuOutput(int choice){
+        switch (choice){
+            case 1:
+                return "CPU chooses...rock!";
+            break;
+
+            case 2:
+                return "CPU chooses...paper!";
+            break;
+
+            case 3:
+                return "CPU chooses...scissors!";
+            break;
+        }
+        return "NULL";
+    }
+    public static void Main (string[] args) {
+        //UI displays choice between CPU or Player
+        Console.WriteLine("Would you like to play against a CPU [1] or a second player? [2]");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        //Check choice
+        if (choice == 1){
+            Console.WriteLine()
+        }
+    }
+}
